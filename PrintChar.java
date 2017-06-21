4c21b664ad8bdbbbd1102c548c950bd77f43df0c
HARIPRIYA R <priyaravichandran1997@gmail.com>
2017-06-21 21:16:50 +0530

    Create PrintChar.java

---
+++ PrintChar.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class PrintChar
+{
+  public static void main(String args[])
+  { 
+     Scanner sc=new Scanner(System.in);
+     System.out.println("ENTER THE STRING");
+     String str=sc.nextLine();
+     for(int i=0;i<str.length();i=i+2)
+     {
+       char c=str.charAt(i);
+       int value=Integer.parseInt(str[i+1]);
+       for(int j=0;j<value;j++)
+         System.out.print(c);
+      }
+    }
+ }
