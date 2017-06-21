5fb43e23deb39e8f72567330df82d9fc5cf2b991
HARIPRIYA R <priyaravichandran1997@gmail.com>
2017-06-21 14:10:09 +0530

    Update Device.java

--- Device.java
+++ Device.java
@@ -4,7 +4,7 @@ public class Device
 {
 public static void main(String args[])
 {
-   int arr[]=new arr[args.length];
+   int arr[]=new int[args.length];
    String ans="";
    for(int i=0;i<args.length;i++)
      arr[i]=Integer.parseInt(args[i]);
