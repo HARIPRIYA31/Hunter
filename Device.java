8730a78d785a4a7afe91e21ef6087926d6243df7
HARIPRIYA R <priyaravichandran1997@gmail.com>
2017-06-21 12:28:55 +0530

    Create Device.java

---
+++ Device.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class Device
+{
+public static void main(String args[])
+{
+   int arr[]=new arr[args.length];
+   String ans="";
+   for(int i=0;i<args.length;i++)
+     arr[i]=Integer.parseInt(args[i]);
+    Arrays.sort(arr);
+    for(int j=1;j<=arr.length;j++)
+    {
+    if(j%3!=0)
+      ans=ans+String.valueOf(arr[j]);
+     else
+      ans=ans+String.valueOf(arr[j])+",";
+     }
+     System.out.println(ans);
+    }
+   }
+   
+   
