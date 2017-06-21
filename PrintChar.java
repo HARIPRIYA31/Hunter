3cdadbc1a528a6aca7be57488c899607341a2ac8
HARIPRIYA R <priyaravichandran1997@gmail.com>
2017-06-21 21:21:58 +0530

    Update PrintChar.java

--- PrintChar.java
+++ PrintChar.java
@@ -9,7 +9,7 @@ public class PrintChar
      for(int i=0;i<str.length();i=i+2)
      {
        char c=str.charAt(i);
-       int value=Integer.parseInt(str[i+1]);
+       int value=Integer.parseInt(str.charAt(i+1));
        for(int j=0;j<value;j++)
          System.out.print(c);
       }
