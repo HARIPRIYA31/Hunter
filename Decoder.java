c4a8fb001cdc91ca065a7ca5218847d1d3515343
HARIPRIYA R <priyaravichandran1997@gmail.com>
2017-06-22 14:32:47 +0530

    Update Decoder.java

--- Decoder.java
+++ Decoder.java
@@ -6,7 +6,7 @@ public class Decoder
       Scanner sc=new Scanner(System.in);
       String str=sc.nextLine();
       String arr[]=str.split("x");
-      int num=(int)arr[0].charAt(arr[0].length()-1);
+      int num=Character.getNumericValue(arr[0].charAt(arr[0].length()-1));
       char value=arr[1].charAt(0);
       String res="";
       for(int i=0;i<arr[0].length()-1;i++)
